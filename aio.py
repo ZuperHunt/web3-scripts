af20710f39329190de48b6423fb69de1e72b6a0c
Naufal <80172679+naufalkharits@users.noreply.github.com>
2024-03-10 16:40:36 +0700

    add Starknet mainnet

--- aio.py
+++ aio.py
@@ -19,6 +19,16 @@ def check_ethereum_balance(wallet_address, rpc_endpoint):
     print(f'Wallet Address: {wallet_address}')
     print(f'Balance: {balance} ETH')
 
+def get_starknet_block(rpc_endpoint):
+    payload = {"jsonrpc":"2.0","method":"starknet_getBlockTransactionCount","params":["latest"],"id":1}
+    response = requests.post(rpc_endpoint, json=payload)
+    result = response.json()['result']
+    return result
+
+def check_starknet_txs(rpc_endpoint):
+    txs = get_starknet_block(rpc_endpoint)
+    print(f'Latest block: {txs} txs')
+
 def get_near_balance(account_id, rpc_endpoint):
     payload = {
         "jsonrpc": "2.0",
@@ -51,6 +61,10 @@ def main():
     ethereum_wallet_address = '0x0000004C4F555675F3043F83b653834359e671c3'
     ethereum_tx_counter = 0
 
+    # Starknet configuration
+    starknet_rpc_endpoint = 'https://rpc.starknet.lava.build/lava-referer-b94ae174-dcf4-49b0-a9ca-8174ae86a1a4/'  # TODO: fill with your Starknet RPC
+    starknet_counter = 0
+
     # NEAR Mainnet Configuration
     near_mainnet_rpc_endpoint = 'https://near.lava.build/lava-referer-630567c6-c700-4955-b1d0-f512e7364026/'
     near_mainnet_account_id = 'lavaman.near'
@@ -68,6 +82,15 @@ def main():
 
         try:
             check_ethereum_balance(ethereum_wallet_address, ethereum_rpc_endpoint)
+        except Exception as e:
+            print("An error occurred:", e)
+        
+        # Starknet
+        starknet_counter += 1
+        print(f"Starknet counter no. {starknet_counter}")
+
+        try:
+            check_starknet_txs(starknet_rpc_endpoint)
         except Exception as e:
             print("An error occurred:", e)
 
